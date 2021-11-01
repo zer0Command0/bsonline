a6634fe4af3f8d74466a15dd027928e70ed85a3a
zer0Command0 <83143017+zer0Command0@users.noreply.github.com>
2021-11-01 16:09:56 +0100

    Setting Up custom Plugin

---
+++ wp-content/plugins/bso-core/bso_core.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: BSOnline Core
+description: A custom plugin made for BSOnline Theme
+Version: 1.0
+Author: ZeroCommunication - Alessio Azzurro
+Author URI: www.zerocommunication.it
+*/
+
